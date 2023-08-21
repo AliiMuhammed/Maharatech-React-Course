d5ec59cbb31bf11bd7a27f60a66ab03732eee317
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-08-21 06:31:19 +0300

    ch2 video 1

---
+++ src/Product.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class Product extends Component {
+    state = {  } 
+    render() { 
+        return (<span>product 1</span>);
+    }
+}
+ 
+export default Product;
